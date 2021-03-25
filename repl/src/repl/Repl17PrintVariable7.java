9650e45f5e3e0ff9c9bfd83271f0d6e0bd7e7dc7
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2021-03-25 03:26:18 +0100

    last update

--- repl/src/repl/Repl17PrintVariable7.java
+++ repl/src/repl/Repl17PrintVariable7.java
@@ -5,16 +5,15 @@ import java.util.Scanner;
 public class Repl17PrintVariable7 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-	    String topic1 = scan.nextLine();
-	    String topic2 = scan.nextLine();
-	    
-	    if (topic1.equals("Java")) {
+		String topic1 = scan.nextLine();
+		String topic2 = scan.nextLine();
+
+		if (topic1.equals("Java")) {
 			System.out.println("I will learn \"" + topic1 + "\" and \"" + topic2 + "\" at CybertekSchool.");
-		}else {
-			System.out.println("I will learn "+ topic1+"and " + topic2+"at CybertekSchool");
+		} else {
+			System.out.println("I will learn " + topic1 + "and " + topic2 + "at CybertekSchool");
 		}
-	    
-	    
+
 	}
 
 }
